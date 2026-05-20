453804101a773869df3f5270af0b42ce746c79ca
wiryanto33 <indrarencongsakti33@gmail.com>
2026-05-20 20:45:09 +0700

    fix:stats widget dipinjam

--- app/Observers/RadioObserver.php
+++ app/Observers/RadioObserver.php
@@ -8,8 +8,8 @@ class RadioObserver
 {
     public function saving(Radio $model): void
     {
-        // Otomatiskan status berdasarkan stok, kecuali jika perbaikan
-        if ($model->status !== Radio::STATUS_PERBAIKAN) {
+        // Jangan timpa status jika radio sedang dipinjam atau dalam perbaikan
+        if (!in_array($model->status, [Radio::STATUS_PERBAIKAN, Radio::STATUS_DIPINJAM])) {
             $model->status = (int) $model->stok <= 0
                 ? Radio::STATUS_STOK_HABIS
                 : Radio::STATUS_TERSEDIA;
